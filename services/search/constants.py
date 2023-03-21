63c08e276ca8aa1fa97d4b75583eb5cefc17b49f
juuso-j <juuso.jokiniemi@turku.fi>
2023-03-21 14:00:10 +0200

    Add HYPHENATE_MODELS constant

--- services/search/constants.py
+++ services/search/constants.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.contrib.gis.gdal import SpatialReference
 
+from mobility_data.models.mobile_unit import MobileUnit
+from services.models import Service, ServiceNode, Unit
+
 LANGUAGES = {k: v.lower() for k, v in settings.LANGUAGES}
 DEFAULT_SRS = SpatialReference(4326)
 SEARCHABLE_MODEL_TYPE_NAMES = (
@@ -11,6 +14,7 @@ SEARCHABLE_MODEL_TYPE_NAMES = (
     "Address",
     "MobileUnit",
 )
+HYPHENATE_MODELS = [Unit, Service, ServiceNode, MobileUnit]
 QUERY_PARAM_TYPE_NAMES = [m.lower() for m in SEARCHABLE_MODEL_TYPE_NAMES]
 # None will slice to the end of list, e.g. no limit.
 DEFAULT_MODEL_LIMIT_VALUE = None
