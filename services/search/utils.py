fe7ba02b487d20f7e35ca4cbde6b6e6bce08d964
juuso-j <juuso.jokiniemi@turku.fi>
2023-03-21 10:26:04 +0200

    Hyphenate only compound words

--- services/search/utils.py
+++ services/search/utils.py
@@ -5,7 +5,6 @@ from django.db.models import Case, When
 from services.models import ServiceNode, ServiceNodeUnitCount, Unit
 from services.search.constants import (
     DEFAULT_TRIGRAM_THRESHOLD,
-    LENGTH_OF_HYPHENATED_WORDS,
     SEARCHABLE_MODEL_TYPE_NAMES,
 )
 
@@ -13,17 +12,24 @@ voikko = libvoikko.Voikko("fi")
 voikko.setNoUglyHyphenation(True)
 
 
+def is_compound_word(word):
+    result = voikko.analyze(word)
+    if len(result) == 0:
+        return False
+    return True if result[0]["WORDBASES"].count("+") > 1 else False
+
+
 def hyphenate(word):
     """
     Returns a list of syllables of the word if word length
     is >= LENGTH_OF_HYPHENATE_WORDS
     """
-    word_length = len(word)
-    if word_length >= LENGTH_OF_HYPHENATED_WORDS:
+    word = word.strip()
+    if is_compound_word(word):
         # By Setting the value to word_length, voikko returns
         # the words that are in the compound word, if the word is
         # not a compound word it returns the syllables as normal.
-        voikko.setMinHyphenatedWordLength(word_length)
+        voikko.setMinHyphenatedWordLength(len(word))
         syllables = voikko.hyphenate(word)
         return syllables.split("-")
     else:
