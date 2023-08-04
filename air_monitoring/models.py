2fd13f820afdcfafd6438dfbe9c534f9e24b79c1
juuso-j <juuso.jokiniemi@turku.fi>
2023-08-04 15:21:15 +0300

    Add initial model

---
+++ air_monitoring/models.py
@@ -0,0 +1,208 @@
+from django.contrib.gis.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.timezone import now
+
+
+class ImportState(models.Model):
+    year_number = models.PositiveSmallIntegerField(null=True)
+    month_number = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)],
+        null=True,
+    )
+    day_number = models.PositiveSmallIntegerField(
+        null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(31)]
+    )
+
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    value = models.FloatField()
+    parameter = models.ForeignKey(
+        "Parameter", on_delete=models.CASCADE, related_name="values"
+    )
+
+    def __str__(self):
+        return "%s %s" % (self.parameter.name, self.value)
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=64)
+    location = models.PointField(srid=4326)
+    geo_id = models.IntegerField()
+    parameters = models.ManyToManyField("Parameter", related_name="stations")
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.location)
+
+    class Meta:
+        ordering = ["id"]
+
+
+class Year(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="years", null=True
+    )
+    year_number = models.PositiveSmallIntegerField(default=2023)
+
+    class Meta:
+        ordering = ["-year_number"]
+        # indexes = [
+        #     models.Index(fields=["station", "year_number"])
+        # ]
+
+    @property
+    def num_days(self):
+        return self.days.count()
+
+    def __str__(self):
+        return "%s, %s" % (self.year_number, self.station.name)
+
+
+class Month(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="months", null=True
+    )
+    year = models.ForeignKey("Year", on_delete=models.CASCADE, related_name="months")
+    month_number = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)], default=1
+    )
+
+    @property
+    def num_days(self):
+        return self.days.count()
+
+    def __str__(self):
+        return "%s" % (self.month_number)
+
+    class Meta:
+        ordering = ["-year__year_number", "-month_number"]
+
+
+class Week(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="weeks"
+    )
+    week_number = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(53)]
+    )
+    years = models.ManyToManyField(Year, related_name="weeks")
+
+    @property
+    def num_days(self):
+        return self.days.count()
+
+    def __str__(self):
+        return "%s" % (self.week_number)
+
+    class Meta:
+        ordering = ["-week_number"]
+
+
+class Day(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="days", null=True
+    )
+    date = models.DateField(default=now)
+    # , db_index=True)
+    weekday_number = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(7)], default=1
+    )
+    week = models.ForeignKey(
+        "Week", on_delete=models.CASCADE, related_name="days", null=True
+    )
+    month = models.ForeignKey(
+        "Month", on_delete=models.CASCADE, related_name="days", null=True
+    )
+    year = models.ForeignKey(
+        "Year", on_delete=models.CASCADE, related_name="days", null=True
+    )
+
+    class Meta:
+        ordering = ["-date"]
+
+
+class Hour(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="hours", null=True
+    )
+    day = models.ForeignKey(
+        "Day", on_delete=models.CASCADE, related_name="hours", null=True
+    )
+    hour_number = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(23)], default=0
+    )
+
+
+class YearData(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="year_datas", null=True
+    )
+    year = models.ForeignKey(
+        "Year", on_delete=models.CASCADE, related_name="year_datas", null=True
+    )
+    measurements = models.ManyToManyField("Measurement", related_name="year_datas")
+
+    class Meta:
+        ordering = ["-year__year_number"]
+
+
+class MonthData(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="month_datas", null=True
+    )
+    month = models.ForeignKey(
+        "Month", on_delete=models.CASCADE, related_name="month_datas", null=True
+    )
+    year = models.ForeignKey(
+        "Year", on_delete=models.CASCADE, related_name="month_datas", null=True
+    )
+    measurements = models.ManyToManyField("Measurement", related_name="month_datas")
+
+    class Meta:
+        ordering = ["-year__year_number", "-month__month_number"]
+
+
+class WeekData(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="week_datas", null=True
+    )
+    week = models.ForeignKey(
+        "Week", on_delete=models.CASCADE, related_name="week_datas", null=True
+    )
+    measurements = models.ManyToManyField("Measurement", related_name="week_datas")
+
+    class Meta:
+        ordering = ["-week__week_number"]
+
+
+class DayData(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="day_datas", null=True
+    )
+    day = models.ForeignKey(
+        "Day", on_delete=models.CASCADE, related_name="day_datas", null=True
+    )
+    measurements = models.ManyToManyField("Measurement", related_name="day_datas")
+
+    class Meta:
+        ordering = ["-day__date"]
+
+
+# Hourly data for a day
+class HourData(models.Model):
+    station = models.ForeignKey(
+        "Station", on_delete=models.CASCADE, related_name="hour_datas"
+    )
+    hour = models.ForeignKey(
+        "Hour", on_delete=models.CASCADE, related_name="hour_datas", null=True
+    )
+    measurements = models.ManyToManyField("Measurement", related_name="hour_datas")
+
+    class Meta:
+        ordering = ["-hour__day__date"]
