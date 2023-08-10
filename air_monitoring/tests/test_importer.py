1e477f5c9dd6a19cda082585d6e5f5e4948ef2b1
juuso-j <juuso.jokiniemi@turku.fi>
2023-08-10 17:50:22 +0300

    Add incremental importing tests

--- air_monitoring/tests/test_importer.py
+++ air_monitoring/tests/test_importer.py
@@ -11,6 +11,7 @@ from air_monitoring.management.commands.constants import (
     PM10_PT1H_AVG,
     START_YEAR,
 )
+from air_monitoring.management.commands.fmi import clear_cache
 from air_monitoring.models import (  # ImportState,; Measurement,; HourData,
     Day,
     DayData,
@@ -81,12 +82,16 @@ def test_importer():
     import_state = ImportState.load()
     assert import_state.year_number == START_YEAR
     assert import_state.month_number == 1
+    clear_cache()
     stations = get_stations()
     save_stations(stations, options["initial_import"])
     assert Station.objects.all().count() == 2
     kaarina_station = Station.objects.get(name=KAARINA_STATION)
-    start_time = dateutil.parser.parse("2021-01-01T00:00:00Z")
-    end_time = dateutil.parser.parse("2021-02-28T23:45:00Z")
+
+    # Always start at the beginning of the month as the incremental
+    # importer imports data monthly
+    start_time = dateutil.parser.parse("2021-11-01T00:00:00Z")
+    end_time = dateutil.parser.parse("2021-12-4T23:45:00Z")
     columns = []
     for station_name in STATION_NAMES:
         for parameter in OBSERVABLE_PARAMETERS:
@@ -99,7 +104,7 @@ def test_importer():
     save_measurements(df, options["initial_import"])
     import_state = ImportState.load()
     assert import_state.year_number == 2021
-    assert import_state.month_number == 2
+    assert import_state.month_number == 12
     # Test year data
     year = Year.objects.get(year_number=2021)
     year_data = YearData.objects.get(station=kaarina_station, year=year)
@@ -110,21 +115,21 @@ def test_importer():
     assert YearData.objects.all().count() == Station.objects.all().count()
 
     # Test month data
-    august = Month.objects.get(year=year, month_number=2)
-    month_data = MonthData.objects.get(station=kaarina_station, month=august)
+    november = Month.objects.get(year=year, month_number=11)
+    month_data = MonthData.objects.get(station=kaarina_station, month=november)
     measurement = month_data.measurements.get(parameter=aqindex_parameter)
     assert round(measurement.value, 1) == 3.0
     assert measurement.parameter.name == AQINDEX_PT1H_AVG
 
     # Test week data
-    week_5 = Week.objects.get(week_number=5, years=year)
-    week_data = WeekData.objects.get(station=kaarina_station, week=week_5)
+    week_46 = Week.objects.get(week_number=46, years=year)
+    week_data = WeekData.objects.get(station=kaarina_station, week=week_46)
     measurement = week_data.measurements.get(parameter=aqindex_parameter)
     assert round(measurement.value, 1) == 3.0
     assert measurement.parameter.name == AQINDEX_PT1H_AVG
 
     # Test day
-    day = Day.objects.get(date=dateutil.parser.parse("2021-02-02T00:00:00Z"))
+    day = Day.objects.get(date=dateutil.parser.parse("2021-11-02T00:00:00Z"))
     day_data = DayData.objects.get(station=kaarina_station, day=day)
     measurement = day_data.measurements.get(parameter=aqindex_parameter)
     assert round(measurement.value, 1) == 3.0
@@ -140,6 +145,47 @@ def test_importer():
     measurement = hour_data.measurements.get(parameter=aqindex_parameter)
     assert round(measurement.value, 1) == 4.0
     assert measurement.parameter.name == AQINDEX_PT1H_AVG
+
+    # Test incremental import
+    clear_cache()
+    options = {"initial_import": False}
+    start_time = dateutil.parser.parse("2021-12-01T00:00:00Z")
+    end_time = dateutil.parser.parse("2021-12-15T23:45:00Z")
+    columns = []
+    for station_name in STATION_NAMES:
+        for parameter in OBSERVABLE_PARAMETERS:
+            columns.append(f"{station_name} {parameter}")
+    df = get_test_dataframe(columns, start_time, end_time)
+    save_parameter_types(df, options["initial_import"])
+    assert Parameter.objects.all().count() == len(OBSERVABLE_PARAMETERS)
+    aqindex_parameter = Parameter.objects.get(name=AQINDEX_PT1H_AVG)
+    assert Parameter.objects.filter(name=PM10_PT1H_AVG).exists() is True
+    save_measurements(df, options["initial_import"])
+    year_data = YearData.objects.get(station=kaarina_station, year=year)
+    measurement = year_data.measurements.get(parameter=aqindex_parameter)
+    assert round(measurement.value, 1) == 3.0
+    assert Year.objects.count() == 1
+    assert Month.objects.count() == 2
+    assert Week.objects.count() == 7
+    assert Day.objects.count() == 30 + 15
+    assert Hour.objects.count() == (30 + 15) * 24
+
+    # Test incremental import when year changes
+    clear_cache()
+    start_time = dateutil.parser.parse("2021-12-01T00:00:00Z")
+    end_time = dateutil.parser.parse("2022-01-15T23:45:00Z")
+    columns = []
+    for station_name in STATION_NAMES:
+        for parameter in OBSERVABLE_PARAMETERS:
+            columns.append(f"{station_name} {parameter}")
+    df = get_test_dataframe(columns, start_time, end_time)
+    save_parameter_types(df, options["initial_import"])
+    assert Parameter.objects.all().count() == len(OBSERVABLE_PARAMETERS)
+    aqindex_parameter = Parameter.objects.get(name=AQINDEX_PT1H_AVG)
+    assert Parameter.objects.filter(name=PM10_PT1H_AVG).exists() is True
+    save_measurements(df, options["initial_import"])
+    year_data = YearData.objects.get(station=kaarina_station, year=year)
+    measurement = year_data.measurements.get(parameter=aqindex_parameter)
 
     # Test initial import
     options = {"initial_import": True}
