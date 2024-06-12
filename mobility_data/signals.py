9dc94c480f784bee79af86fe6d0d8e429e1d056c
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-06-12 09:56:00 +0300

    Update mobile unit search columns

--- mobility_data/signals.py
+++ mobility_data/signals.py
@@ -1,9 +1,11 @@
 from django.core import management
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from mobility_data.constants import DATA_SOURCE_IMPORTERS
-from mobility_data.models import DataSource
+from mobility_data.models import DataSource, MobileUnit
+from services.signals import generate_syllables, populate_search_column
 
 
 @receiver(post_save, sender=DataSource)
@@ -15,3 +17,11 @@ def data_source_on_save(sender, **kwargs):
             management.call_command("turku_services_import", importer["importer_name"])
         else:
             management.call_command(f"import_{importer['importer_name']}")
+
+
+@receiver(post_save, sender=MobileUnit)
+def mobile_unit_on_save(sender, **kwargs):
+    obj = kwargs["instance"]
+    generate_syllables(obj)
+    # Do transaction after successfull commit.
+    transaction.on_commit(populate_search_column(obj))
