e1dc353c7b1a3d6a25f80f0475b4575e6284b1ac
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-06-17 08:59:54 +0300

    Add constants for excluding content types

--- services/search/constants.py
+++ services/search/constants.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib.gis.gdal import SpatialReference
 
+from mobility_data.models.content_type import ContentType
+
 LANGUAGES = {k: v.lower() for k, v in settings.LANGUAGES}
 DEFAULT_SRS = SpatialReference(4326)
 SEARCHABLE_MODEL_TYPE_NAMES = (
@@ -20,3 +22,25 @@ DEFAULT_TRIGRAM_THRESHOLD = 0.15
 DEFAULT_RANK_THRESHOLD = 1
 
 HYPHENATE_ADDRESSES_MODIFIED_WITHIN_DAYS = 7
+EXCLUDE_CONTENT_TYPE_NAMES = [
+    "FoliStop",
+    "StreetAreaInformation",
+    "TicketMachineSign",
+    "TaxiStandSign",
+    "SingleTrackRailwayLevelCrossingSign",
+    "RailwayLevelCrossingWithBoomsSign",
+    "RailwayLevelCrossingWithoutBoomsSign" "ParkingLotSign",
+    "PaidParkingSig",
+    "ObligationToUseParkingDiscSign",
+    "ParkingForbiddenAreaSign",
+    "ParkingForbiddenSign",
+    "LongDistanceBusStopSign",
+    "LocalTrafficBusStopSign",
+    "ParkingTerminalSign",
+    "CrossWalkSign",
+    "RouteForDisabledSign",
+    "DisabledParkingSign",
+]
+EXCLUDE_CONTENT_TYPES = ContentType.objects.filter(
+    type_name__in=EXCLUDE_CONTENT_TYPE_NAMES
+)
