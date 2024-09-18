fec6ada47a7174c0e8c6dbe0cf61399eeb7f558c
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-09-18 11:38:14 +0300

    Add API from street_maintenace application

---
+++ maintenance/api/serializers.py
@@ -0,0 +1,64 @@
+from django.contrib.gis.geos import LineString, Point
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
+from rest_framework import serializers
+
+from maintenance.models import GeometryHistory, MaintenanceUnit, MaintenanceWork
+
+
+class GeometryHistorySerializer(serializers.ModelSerializer):
+    geometry_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = GeometryHistory
+        fields = [
+            "id",
+            "geometry_type",
+            "events",
+            "timestamp",
+            "provider",
+            # Removed for permormance issues as it is not currently used
+            # "geometry",
+            "coordinates",
+        ]
+
+    @extend_schema_field(OpenApiTypes.STR)
+    def get_geometry_type(self, obj):
+        return obj.geometry.geom_type
+
+
+class ActiveEventSerializer(serializers.Serializer):
+    events = serializers.CharField(max_length=64)
+
+
+class MaintenanceUnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MaintenanceUnit
+        fields = "__all__"
+
+
+class MaintenanceWorkSerializer(serializers.ModelSerializer):
+    provider = serializers.PrimaryKeyRelatedField(
+        many=False, source="maintenance_unit.provider", read_only=True
+    )
+
+    class Meta:
+        model = MaintenanceWork
+        fields = [
+            "id",
+            "maintenance_unit",
+            "provider",
+            "geometry",
+            "timestamp",
+            "events",
+            "original_event_names",
+        ]
+
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        if isinstance(obj.geometry, Point):
+            representation["lat"] = obj.geometry.y
+            representation["lon"] = obj.geometry.x
+        elif isinstance(obj.geometry, LineString):
+            representation["coords"] = obj.geometry.coords
+        return representation
---
+++ maintenance/api/urls.py
@@ -0,0 +1,24 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from . import views
+
+app_name = "maintenance"
+
+router = routers.DefaultRouter()
+router.register("active_events", views.ActiveEventsViewSet, basename="active_events")
+
+router.register(
+    "maintenance_works", views.MaintenanceWorkViewSet, basename="maintenance_works"
+)
+router.register(
+    "maintenance_units", views.MaintenanceUnitViewSet, basename="maintenance_units"
+)
+
+router.register(
+    "geometry_history", views.GeometryHitoryViewSet, basename="geometry_history"
+)
+
+urlpatterns = [
+    path("", include(router.urls), name="maintenance"),
+]
