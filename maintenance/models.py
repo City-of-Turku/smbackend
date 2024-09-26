1782b2d859ab1029be74e2f5b7ee8e221ad5a6ca
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-09-26 13:29:23 +0300

    Add models for storing Unit maintenance history

--- maintenance/models.py
+++ maintenance/models.py
@@ -2,8 +2,55 @@ from django.contrib.gis.db import models
 from django.contrib.postgres.fields import ArrayField
 
 from maintenance.management.commands.constants import PROVIDER_CHOICES
+from services.models import Unit
 
 DEFAULT_SRID = 4326
+
+
+class UnitMaintenance(models.Model):
+    """
+    Model for storing maintenance information for service_unit entities.
+    """
+
+    UNDEFINED = "UNDEFINED"
+    USABLE = "USABLE"
+    UNUSABLE = "UNUSABLE"
+    CONDITION_CHOICES = [
+        (UNDEFINED, "undefined"),
+        (USABLE, "usable"),
+        (UNUSABLE, "unusable"),
+    ]
+    SKI_TRAIL = "SKI_TRAIL"
+    ICE_TRACK = "ICE_TRACK"
+    TARGET_CHOICES = [(SKI_TRAIL, "ski_trail"), (ICE_TRACK, "ice_track")]
+    condition = models.CharField(
+        max_length=16, choices=CONDITION_CHOICES, default=UNDEFINED
+    )
+    target = models.CharField(max_length=16, choices=TARGET_CHOICES, default=SKI_TRAIL)
+    maintained_at = models.DateTimeField()
+    last_imported_time = models.DateTimeField(help_text="Time of last data import")
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE,
+        related_name="maintenance",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ["-maintained_at"]
+
+
+class UnitMaintenanceGeometry(models.Model):
+    geometry_id = models.IntegerField(unique=True, blank=True, null=True)
+    geometry = models.GeometryField(srid=DEFAULT_SRID, null=True)
+    unit_maintenance = models.ForeignKey(
+        UnitMaintenance,
+        on_delete=models.SET_NULL,
+        related_name="geometries",
+        null=True,
+        blank=True,
+    )
 
 
 class MaintenanceUnit(models.Model):
