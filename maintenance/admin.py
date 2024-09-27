e8bc4063bf1ae2dc72da8c385863788acf37770d
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-09-27 11:43:04 +0300

    Add UnitMaintenance and UnitMaintenanceGeometry

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from maintenance.models import MaintenanceUnit, MaintenanceWork
+from maintenance.models import (
+    MaintenanceUnit,
+    MaintenanceWork,
+    UnitMaintenance,
+    UnitMaintenanceGeometry,
+)
 
 admin.site.register(MaintenanceWork)
 admin.site.register(MaintenanceUnit)
+admin.site.register(UnitMaintenance)
+admin.site.register(UnitMaintenanceGeometry)
