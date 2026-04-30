ab618ec3d5f42a18fe0aaa807d035939ab85d624
Juho Turkki <juho.turkki@gofore.com>
2026-04-30 13:00:53 +0300

    black reformat

--- mobility_data/tests/test_import_foli_parkandride_stops.py
+++ mobility_data/tests/test_import_foli_parkandride_stops.py
@@ -69,9 +69,7 @@ def test_import_foli_stops(fetch_json_mock, fetch_json_with_headers_mock):
     bikes_stops_content_type.name_sv = config["name"]["sv"]
     bikes_stops_content_type.name_en = config["name"]["en"]
     # Fixture data contains two park and ride stops for cars and bikes.
-    assert (
-        MobileUnit.objects.filter(content_types=cars_stops_content_type).count() == 7
-    )
+    assert MobileUnit.objects.filter(content_types=cars_stops_content_type).count() == 7
     assert (
         MobileUnit.objects.filter(content_types=bikes_stops_content_type).count() == 7
     )
@@ -97,7 +95,5 @@ def test_import_foli_stops(fetch_json_mock, fetch_json_with_headers_mock):
     num_created, num_deleted = save_to_database(car_stops, content_type)
     assert num_created == 0
     assert num_deleted == 0
-    assert (
-        MobileUnit.objects.filter(content_types=cars_stops_content_type).count() == 7
-    )
+    assert MobileUnit.objects.filter(content_types=cars_stops_content_type).count() == 7
     assert hirvensalo.id == MobileUnit.objects.filter(name_en="Hirvensalo").first().id
